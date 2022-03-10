b6421a5bd137cbcee96ea0271ee2488c8e445c9a
Martin Cometta <56566511+mrtn-c@users.noreply.github.com>
2022-03-10 09:57:10 -0300

    Create Alumnos.java

---
+++ Alumnos.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Alumnos {
+    public static void main (String[] args) {
+        
+        String [] alumnos = new String [1000];
+        String [] ciudad = new String [1000];
+        int i = 0, cant = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        
+        for (i=0; i<1000; i++){
+            System.out.printf("Ingrese el nombre del alumno %d : ", i+1 );
+            alumnos[i] = scanner.nextLine();
+            System.out.printf("Ingrese la ciudad del alumno %d -> %s : ", i+1, alumnos[i] );
+            ciudad[i] = scanner.nextLine();
+            cant+=1;
+            System.out.println("Si desea no agregar mas personas presione 1");
+            int r = scanner.nextInt();
+            if (r == 1){
+                i = 1000;
+            }
+
+        }    
+
+    }
+
+    private void buscarAlumno(int cant) {
+        
+        
+    }
+}
